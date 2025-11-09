2400dde8dd56d9f0867e9f8d644be675af6dedd2
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2025-11-09 11:47:09 -0800

    feat: first upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['attribute', 'data-theme'],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
